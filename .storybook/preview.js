2a0d64f944875ed8b16b11c34f0a0a9e72f44ae4
ryuilhwan <ih.ryu.dev@gmail.com>
2023-06-26 17:58:48 +0900

    visual test

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,10 @@
 import "../src/index.css";
-
+import { initialize, mswDecorator } from 'msw-storybook-addon';
 /** @type { import('@storybook/react').Preview } */
+initialize()
+
 const preview = {
+  decorators: [mswDecorator],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
